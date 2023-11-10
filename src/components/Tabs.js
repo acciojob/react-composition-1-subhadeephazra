7828bef4edf5452873800554a13d33ac97b49810
Subhadeep Hazra <124555668+subhadeephazra@users.noreply.github.com>
2023-11-10 09:42:55 +0000

    UI changes

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 
-const Tabs = ({tabprop}) => {
-const [content, setContent] = useState('');
+const Tabs = ({ tabprop }) => {
+  const [content, setContent] = useState('');
 
   return (
     <div>
-        <ul>
-            {tabprop.map((tab) =>{
-                return <li onclick={()=>setContent(tab.content)}>{tab.title}</li>
-            })}
-        </ul>
-        <p>{content} hi</p>
+      <ul>
+        {tabprop.map((tab) => (
+          <li key={tab.title} onClick={() => setContent(tab)}>
+            {tab.title}
+          </li>
+        ))}
+      </ul>
+      <p>This is the content for {content.title}</p>
     </div>
   );
 };
